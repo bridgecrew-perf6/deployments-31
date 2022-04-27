0fe38522d45a2e9caf33cf5b08e919792bdb40a5
Oi Shadmon <oshadmon@gmail.com>
2022-04-26 20:10:32 -0700

    invalid

--- helm/credentials.sh
+++
@@ -1,6 +0,0 @@
-DOCKER_REGISTRY_SERVER=docker.io
-kubectl create secret docker-registry git-imagepullsecret \
-  --docker-server=${DOCKER_REGISTRY_SERVER} \
-  --docker-username=\$oauthtoken \
-  --docker-password=a83691ce-df29-46e2-8c0e-00ff8a97e5b4 \
-
