93fc226910db5a407294c6b22e3299db35992294
Oi Shadmon <oshadmon@gmail.com>
2022-04-30 16:20:25 -0700

    queries

--- queries.sh
+++ queries.sh
@@ -12,39 +12,40 @@ printf "\n"
 #printf "\n"
 #printf "\n"
 
-
+read -p ""
 read -p "From a single point -- show INGESTION status"
 printf "\n"
 curl -X GET localhost:32349 -H "command: get streaming" -H "User-Agent: AnyLog/1.23" -H "service: sync" -H "destination: ${DESTINATIONS}"
 printf "\n"
 printf "\n"
 
+read -p ""
 read -p "From a single point --  query metadata (network members)"
 printf "\n"
 curl -X GET localhost:32349 -H 'command: blockchain get (master, operator, query) bring [*][name] \t - \t [*][local_ip] : [*][port]  separator=\n' -H "User-Agent: AnyLog/1.23" -w "\n"
 printf "\n"
 printf "\n"
 
-read -p "From a single point -- query metadata (nodes hosting  device and insight data)"
+read -p "From a single point -- query metadata (nodes hosting device and insight data)"
 printf "\n"
 curl -X GET localhost:32349 -H "command: get data nodes" -H "User-Agent: AnyLog/1.23"
 printf "\n"
 printf "\n"
 
-read -p "From a single point -- show data tables in the network (as a single machine)"
+read -p "From a single point -- show data tables in the network"
 printf "\n"
 curl -X GET localhost:32349 -H "command: get tables where dbms=*" -H "User-Agent: AnyLog/1.23"
 printf "\n"
 printf "\n"
 
-
-read -p "From a single point -- show columns in a specific table"
+read -p ""
+read -p "From a single point -- show columns in a table"
 printf "\n"
 curl -X GET localhost:32349 -H "command: get columns where table=plc_sensor and dbms=nvidia" -H "User-Agent: AnyLog/1.23"
 printf "\n"
 printf "\n"
 
-
+read -p ""
 read -p "From a single point -- query the last 15 minutes of PLC data"
 printf "\n"
 curl -X GET localhost:32349 -H 'command: sql nvidia format=table and extend=(+ip, +node_name) "select timestamp, value FROM plc_sensor WHERE timestamp >= NOW() - 15 minutes ORDER BY value"' -H "User-Agent: AnyLog/1.23" -H "destination: network"
@@ -52,26 +53,28 @@ printf "\n"
 printf "\n"
 
 
-
+read -p ""
 read -p "From a single point -- query statistics from the last 15 minutes"
 printf "\n"
 curl -X GET localhost:32349 -H 'command: sql nvidia format=table "select min(value), max(value), avg(value)  FROM plc_sensor WHERE timestamp >= NOW() - 15 minutes"' -H "User-Agent: AnyLog/1.23" -H "destination: network"
 printf "\n"
 printf "\n"
 
-
+read -p ""
 read -p "From a single point -- query TRAFFIC INSIGHTS over the past month"
 printf "\n"
 curl -X GET localhost:32349 -H 'command: sql nvidia format=table "select intersection, timestamp, speed FROM traffic_data ORDER BY timestamp"' -H "User-Agent: AnyLog/1.23" -H "destination: network"
 printf "\n"
 printf "\n"
 
-read -p "Destination Nodes"
+read -p ""
+read -p "Source Nodes"
 printf "\n"
 curl -X GET localhost:32349 -H "command: query status" -H "User-Agent: AnyLog/1.23"
 printf "\n"
 printf "\n"
 
+read -p ""
 read -p "From a single point -- 101 N X University Ave."
 printf "\n"
 DESTINATION=$(curl -X GET localhost:32349 -H 'command: blockchain get operator where name=nvidia-nodeA bring [operator][local_ip] : [operator][port] ' -H "User-Agent: AnyLog/1.23" -w "\n" 2> /dev/null)
@@ -79,7 +82,16 @@ curl -X GET localhost:32349 -H "command: sql nvidia format=table select intersec
 printf "\n"
 printf "\n"
 
-read -p "Destination Nodes"
+read -p ""
+read -p "Source Nodes"
 printf "\n"
 curl -X GET localhost:32349 -H "command: query status" -H "User-Agent: AnyLog/1.23"
 printf "\n"
+
+read -p ""
+read -p "From a single point -- 101 N X University Ave. - Speed Info at a particular time"
+printf "\n"
+curl -X GET localhost:32349 -H "command: sql nvidia format=table select min(speed), max(speed), avg(speed) FROM traffic_data WHERE intersection='101 N / University Ave' and timestamp >= '2022-04-24 00:00:00' and timestamp <= '2022-04-24 23:59:59'" -H "User-Agent: AnyLog/1.23" -H "destination: ${DESTINATION}"
+printf "\n"
+printf "\n"
+
